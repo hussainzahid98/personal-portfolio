25fc4adceda1f39f28a1bdc75019992b7ad1a083
hussainzahidgiki <hussainzahidgiki@gmail.com>
2023-02-25 00:29:11 +0500

    added new image

--- src/portfolio.js
+++ src/portfolio.js
@@ -249,7 +249,7 @@ const testimonials = {
   subtitle: "Some Awesome people I've worked with",
   projects: [
     {
-      image: require("./assets/images/Rohan.jpeg"),
+      image: require("./assets/images/Rohan2.jpeg"),
       projectName: "Rohan de Launey",
       jobTitle: "Co-founder Inosio",
       jobLocation: "📍 San Diego, CA",
